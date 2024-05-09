1dd7104743876f58c5e93d73afd5b3fae81458f7
Julian Mierzejewski <j3mierz@gmail.com>
2024-05-09 14:52:49 +0200

    rooms table added

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Rooms(models.Model):
+    name = models.CharField(max_length=50)
+    seats = models.IntegerField()
+    projector = models.BooleanField()
