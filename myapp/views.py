3dbb2f877f4ae4eb7041de34a6636b8dee018745
Julian Mierzejewski <j3mierz@gmail.com>
2024-05-09 14:53:48 +0200

    home page and adding rooms added

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+
+from myapp.models import Rooms
+
 
 # Create your views here.
+
+class HomePage(View):
+    def get(self, request):
+        return render(request, 'home_page.html', {'rooms': Rooms.objects.all()})
+
+    def post(self, request):
+        pass
+
+class AddRoom(View):
+    def get(self, request):
+        return render(request, 'add_room.html')
+    def post(self, request):
+        projector = False
+        name = request.POST.get('name')
+        seats = request.POST.get('seats')
+        if request.POST.get('projector') == "on":
+            projector = True
+        Rooms.objects.create(name=name, seats=seats, projector=projector)
+        return redirect('home')
+
