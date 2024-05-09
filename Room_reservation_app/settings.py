066b8d866ebe44c77f24b3949586a4a72329ad05
Julian Mierzejewski <j3mierz@gmail.com>
2024-05-09 14:54:47 +0200

    settings

--- Room_reservation_app/settings.py
+++ Room_reservation_app/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'Room_reservation_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'rooms_app',
+        'USER': 'postgres',
+        'PASSWORD': 'coderslab',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
